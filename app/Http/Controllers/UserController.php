8ae99e5a164ab5918994d050a2ae99bf36538447
mazrofiq <mazrofiq@gmail.com>
2025-09-09 14:21:36 +0800

    va dipc

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,30 +67,30 @@ class UserController extends Controller
         
 
 
-        // $Body = ['responseCode' => '2002400',
-        //             'responseMessage' => 'Successful',
-        //             'virtualAccountData' => [
-        //                 'partnerServiceId' => $partner_serviceId,
-        //                 'customerNo' => $cust_no,
-        //                 'virtualAccountNo' => $va_number,
-        //                 'virtualAccountName' => 'test',
-        //                 'totalAmount' => [
-        //                     'value' => '0.00',
-        //                     'currency' => 'IDR'
-        //                 ],
-        //                 'virtualAccountTrxType' => 'O',
-        //                 'inquiryRequestId' => 'ord-'.$order
-        //             ],
-        //             'additionalInfo' => [
-        //                 'channel' => $channel,
-        //                 'trxId' => 'inv-'.$order
-        //             ]
-        //         ]
-        //         ;
-        $Body = ['responseCode' => '4042419',
-                    'responseMessage' => 'Bill Expired'
+        $Body = ['responseCode' => '2002400',
+                    'responseMessage' => 'Successful',
+                    'virtualAccountData' => [
+                        'partnerServiceId' => $partner_serviceId,
+                        'customerNo' => $cust_no,
+                        'virtualAccountNo' => $va_number,
+                        'virtualAccountName' => 'test',
+                        'totalAmount' => [
+                            'value' => '0.00',
+                            'currency' => 'IDR'
+                        ],
+                        'virtualAccountTrxType' => 'O',
+                        'inquiryRequestId' => 'ord-'.$order
+                    ],
+                    'additionalInfo' => [
+                        'channel' => $channel,
+                        'trxId' => 'inv-'.$order
+                    ]
                 ]
                 ;
+        // $Body = ['responseCode' => '4042412',
+        //             'responseMessage' => 'Bill not found'
+        //         ]
+        //         ;
         echo json_encode($Body);
     }
 }

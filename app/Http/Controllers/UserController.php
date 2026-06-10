2314fd3f1138826744c6ebbfdd5c27a0dc2cfac0
mazrofiq <mazrofiq@gmail.com>
2026-06-10 11:45:44 +0800

    update param

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
-    // private $clientId;
-    // private $baseUrl;
+    private $clientId;
+    private $baseUrl;
 
-    // public function __construct()
-    // {
-    //     $this->clientId = env('DOKU_CLIENT_ID');
-    //     $this->baseUrl = env('DOKU_BASE_URL');
-    // }
+    public function __construct()
+    {
+        $this->clientId = env('DOKU_CLIENT_ID');
+        $this->baseUrl = env('DOKU_BASE_URL');
+    }
     private function renderPaymentResult(array $responseData, string $invoiceNumber = '')
     {
         if (
@@ -186,8 +186,8 @@ class UserController extends Controller
     public function charge(Request $request){
         // $clientId = 'BRN-0242-1763721186902xx';
         // $secretKey = 'SK-k0Sklx8ZZCqlZpOyPDq7';
-        // $clientId = 'BRN-0118-1780559864110
-        // $secretKey = SK-ln85qKqxFQ6QYmqH2Uc0
+        $clientId = 'BRN-0118-1780559864110';
+        $secretKey = 'SK-ln85qKqxFQ6QYmqH2Uc0';
         // dd($request->all());
         $invoiceNumber = "INV-" . time();
         $targetPath = '/credit-card/charge';
@@ -210,49 +210,12 @@ class UserController extends Controller
             ]
         ];
 
-        // $signData = dokuApiRequest(
-        //     $body,
-        //     $targetPath
-        // );
-
-        $clientId = env('DOKU_CLIENT_ID');
-        $secretKey = env('DOKU_SECRET_KEY');
-        $getUrl = env('DOKU_BASE_URL');
-        $requestId = (string) Str::uuid();
-        $timestamp = gmdate('Y-m-d\TH:i:s\Z');
-        dd($clientId);
-
-        $digest = base64_encode(
-            hash('sha256', json_encode($body), true)
+        $signData = dokuApiRequest(
+            $body,
+            $targetPath
         );
 
-        $stringToSign =
-            "Client-Id:$clientId\n" .
-            "Request-Id:$requestId\n" .
-            "Request-Timestamp:$timestamp\n" .
-            "Request-Target:$targetPath\n" .
-            "Digest:$digest";
-
-        $signature = "HMACSHA256=" . base64_encode(
-            hash_hmac(
-                'sha256',
-                $stringToSign,
-                $secretKey,
-                true
-            )
-        );
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Client-Id' => $clientId,
-            'Request-Id' => $requestId,
-            'Request-Timestamp' => $timestamp,
-            'Signature' => $signature,
-        ])->post(
-            $getUrl.$targetPath,
-            $body
-        );
-
-        $responseData = $response->json();
+        $responseData = $signData->json();
         // dd($responseData);
         return $this->renderPaymentResult(
             $responseData,
